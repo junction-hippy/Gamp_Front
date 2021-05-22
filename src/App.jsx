296e050ecd949385fc059a5fbb400439329bf6ac
Inseo Hwang <sjsjsj1246@gmail.com>
2021-05-23 01:22:50 +0900

    🎨 voice chat

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import ChimeSdkWrapper from './lib/ChimeSdkWrapper';
 import * as config from './config';
 import Welcome from './components/Welcome';
 import ChatTest from './components/ChatTest';
+import Home from './components/chat/Welcome';
+import Meeting from './components/chat/Meeting';
 
 function App() {
   const chime = new ChimeSdkWrapper();
@@ -18,11 +20,17 @@ function App() {
       <Header />
       <Route path="/" exact render={() => <SelectGamePage chime={chime} />} />
       <Route path="/chat" render={() => <ChattingPage chime={chime} />} />
-      <Route path="/test" render={() => <Welcome chime={chime} />} />
+      {/* <Route path="/test" render={() => <Welcome chime={chime} />} />
       <Route
         path={`${baseHref}/meeting`}
         render={() => <ChatTest chime={chime} />}
-      />
+      /> */}
+      <Route path={`${baseHref}/meeting`}>
+        <Meeting chime={chime} />
+      </Route>
+      <Route path={`${baseHref}/test`}>
+        <Home chime={chime} />
+      </Route>
     </>
   );
 }
--- src/components/ChatTest.jsx
+++ src/components/ChatTest.jsx
@@ -9,89 +9,87 @@ import { MeetingSessionStatusCode } from 'amazon-chime-sdk-js';
 
 function ChatTest({ chime }) {
   const baseHref = config.BASE_HREF;
-  //   const [ssName, setSsName] = useState('');
   const audioElementRef = useRef();
 
   const history = useHistory();
   const location = useLocation();
-  const [meetingStatus, setMeetingStatus] = useState('');
-  let ssName = '';
-  let title = '';
-  let role = '';
-  let username = '';
-  let joinInfo = {};
-  const [roster, setRoster] = useState([]);
-  const [rosterChanged, setRosterChanged] = useState(false);
-  let previousRoster = {};
-
-  const start = async () => {
-    try {
-      const qs = new URLSearchParams(location.search);
-      const room = qs.get('room');
-      ssName = `chime[${room}]`;
-      if (!room || !sessionStorage.getItem(ssName)) {
-        history.push(`${baseHref}/test`);
-      }
-
-      const ssData = JSON.parse(sessionStorage.getItem(ssName));
-      console.log(ssData);
-      username = ssData.username;
-      title = ssData.title;
-      role = ssData.role;
-
-      if (!ssData.joinInfo) {
-        const response = await chime.createRoom(
-          role,
-          username,
-          title,
-          ssData.playbackURL,
-        );
-        console.log(response);
-        joinInfo = response;
-        console.log(joinInfo);
-        const data = {
-          ...ssData,
-          joinInfo: joinInfo,
-        };
-        sessionStorage.setItem(ssName, JSON.stringify(data));
-      } else {
-        // Browser refresh
-        joinInfo = ssData.joinInfo;
-        await chime.reInitializeMeetingSession(joinInfo, username);
-      }
-
-      setMeetingStatus('Success');
-      console.log(meetingStatus);
-
-      chime.audioVideo.addObserver({
-        audioVideoDidStop: async (sessionStatus) => {
-          if (
-            sessionStatus.statusCode() ===
-            MeetingSessionStatusCode.AudioCallEnded
-          ) {
-            const whereTo = `${baseHref}/${role === 'host' ? '' : 'end'}`;
-            chime.leaveRoom(role === 'host');
-            history.push(whereTo);
-          }
-        },
-      });
-
-      await chime.joinRoom(audioElementRef.current);
-    } catch (error) {
-      // eslint-disable-next-line
-      console.error(error);
-      setMeetingStatus('Failed');
-    }
-  };
+  const meetingStatus = useRef('');
+  const ssName = useRef('');
+  const title = useRef('');
+  const role = useRef('');
+  const username = useRef('');
+  const joinInfo = useRef({});
 
   //chime setting
   useEffect(() => {
+    const start = async () => {
+      try {
+        const qs = new URLSearchParams(location.search);
+        const room = qs.get('room');
+        ssName.current = `chime[${room}]`;
+        if (!room || !sessionStorage.getItem(ssName.current)) {
+          history.push(`${baseHref}/test`);
+        }
+
+        const ssData = JSON.parse(sessionStorage.getItem(ssName.current));
+        console.log(ssData);
+        username.current = ssData.username;
+        title.current = ssData.title;
+        role.current = ssData.role;
+
+        if (!ssData.joinInfo) {
+          const response = await chime.createRoom(
+            role,
+            username,
+            title,
+            ssData.playbackURL,
+          );
+          console.log(response);
+          joinInfo.current = response;
+          console.log(joinInfo);
+          const data = {
+            ...ssData,
+            joinInfo: joinInfo,
+          };
+          sessionStorage.setItem(ssName, JSON.stringify(data));
+        } else {
+          // Browser refresh
+          joinInfo.current = ssData.joinInfo;
+          await chime.reInitializeMeetingSession(joinInfo, username);
+        }
+
+        meetingStatus.current = 'Success';
+        console.log(meetingStatus.current);
+
+        chime.audioVideo.addObserver({
+          audioVideoDidStop: async (sessionStatus) => {
+            if (
+              sessionStatus.statusCode() ===
+              MeetingSessionStatusCode.AudioCallEnded
+            ) {
+              const whereTo = `${baseHref}/${role === 'host' ? '' : 'end'}`;
+              chime.leaveRoom(role === 'host');
+              history.push(whereTo);
+            }
+          },
+        });
+
+        await chime.joinRoom(audioElementRef.current);
+      } catch (error) {
+        // eslint-disable-next-line
+        console.error(error);
+        meetingStatus.current = 'Failed';
+      }
+    };
     start();
-  }, []);
+  }, [baseHref, history, chime, location]);
 
   return (
     <div className="App-body">
       <audio ref={audioElementRef} style={{ display: 'none' }} />
+      {/* {meetingStatus === 'Success' && (
+        <RemoteVideoGroup joinInfo={this.joinInfo} />
+      )} */}
     </div>
   );
 }
